9dbf852581371eb4e478c0d9a1d3cc2275f37771
ARYAN DOGRA <dograwarrior2003@gmail.com>
2025-07-14 02:06:19 +0530

    login logout and update profile api

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import cloudinary from "../lib/cloudinary.js";
 import { generateToken } from "../lib/ustils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
@@ -5,6 +6,10 @@ import bcrypt from "bcryptjs";
 export const signup = async (req, res) => {
   try {
     const { email, fullName, password, profilePic } = req.body;
+    // Validate input
+    if (!fullName || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
     if (password.length < 6) {
       return res.status(400).json({
         message: "Password must be at least 6 characters long",
@@ -50,10 +55,81 @@ export const signup = async (req, res) => {
   }
 };
 
-export const login = (req, res) => {
-  res.send(`login Page`);
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      res.status(400).json({
+        message: "invalid email or password",
+      });
+    }
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(400).json({
+        message: "invalid email or password",
+      });
+    }
+    // generate jwt token here
+    generateToken(user._id, res);
+    res.status(200).json({
+      message: "Login successful",
+      user: {
+        _id: user._id,
+        fullName: user.fullName,
+        email: user.email,
+        profilePic: user.profilePic,
+      },
+    });
+  } catch (error) {
+    console.error("Error during login:", error.message);
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
 };
 
 export const logout = (req, res) => {
-  res.send(`logout Page`);
+  try {
+    res.cookie("jwt", "", { maxAge: 0 }); // Clear the cookie
+    res.status(200).json({
+      message: "Logged out successfully",
+    });
+  } catch (error) {
+    console.error("Error during logout:", error.message);
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
+
+export const updateProfile = async (req, res) => {
+  try {
+    const { profilePic } = req.body;
+    const userId = req.user._id;
+    if (!profilePic) {
+      return res.status(400).json({ message: "Profile picture is required" });
+    }
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { profilePic: uploadResponse.secure_url },
+      { new: true }
+    );
+    res.status(200).json({
+      message: "Profile updated successfully",
+      updatedUser,
+    });
+  } catch (error) {}
+};
+
+export const checkAuth = (req, res) => {
+  try {
+    res.status(200).json(req.user);
+  } catch (error) {
+    console.error("Error during authentication check:", error.message);
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
 };
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 
@@ -9,6 +10,8 @@ const PORT = process.env.PORT;
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
+app.use(cookieParser());
+
 app.use(`/api/auth`, authRoutes);
 
 app.listen(PORT, () => {
