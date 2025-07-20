71a8d483bc5930e294d9abf1b6c39bc44e13ae57
ARYAN DOGRA <dograwarrior2003@gmail.com>
2025-07-20 17:33:43 +0530

    project updated

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
@@ -12,9 +13,15 @@ const PORT = process.env.PORT;
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use(`/api/auth`, authRoutes);
-app.use(`/api/message`, messageRoutes);
+app.use(`/api/messages`, messageRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,80 @@
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios";
+import toast from "react-hot-toast";
+
+export const useAuthStore = create((set) => ({
+  authUser: null,
+  isSigningup: false,
+  isLoggingIn: false,
+  isUpdatingProfile: false,
+  isCheckingAuth: true,
+  onlineUsers: [],
+
+  checkAuth: async () => {
+    try {
+      const response = await axiosInstance.get("/auth/check");
+      set({ authUser: response?.data });
+    } catch (error) {
+      console.error("Error checking authentication:", error);
+      set({ authUser: null });
+    } finally {
+      set({ isCheckingAuth: false });
+    }
+  },
+  signup: async (data) => {
+    set({ isSigningUp: true });
+    try {
+      const res = await axiosInstance.post("/auth/signup", data);
+      set({ authUser: res?.data });
+      toast.success("Signup successful!");
+    } catch (error) {
+      toast.error(
+        error.response?.data?.message || "An error occurred during signup"
+      );
+    } finally {
+      set({ isSigningUp: false });
+    }
+  },
+
+  login: async (data) => {
+    set({ isLoggingIn: true });
+    try {
+      const res = await axiosInstance.post("/auth/login", data);
+      set({ authUser: res?.data });
+      toast.success("Logged in successfully");
+
+      get().connectSocket();
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isLoggingIn: false });
+    }
+  },
+
+  logout: async () => {
+    try {
+      await axiosInstance.post("/auth/logout");
+      set({ authUser: null });
+      toast.success("Logged out successfully!");
+    } catch (error) {
+      toast.error(
+        error.response?.data?.message || "An error occurred during logout"
+      );
+    }
+  },
+  updateProfile: async (data) => {
+    set({ isUpdatingProfile: true });
+    try {
+      const res = await axiosInstance.put("/auth/update-profile", data);
+      set({ authUser: res?.data });
+      toast.success("Profile updated successfully!");
+    } catch (error) {
+      toast.error(
+        error.response?.data?.message ||
+          "An error occurred while updating profile"
+      );
+    } finally {
+      set({ isUpdatingProfile: false });
+    }
+  },
+}));
